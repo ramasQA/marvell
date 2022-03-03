bc5801ac0458460c781e176fe44328a5250ababe
sergioQA <sshamotenko@gmail.com>
2022-03-04 00:53:16 +0700

    comment

---
+++ starting-template/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+     <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand">Marwell</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="поиск..." aria-label="Search">
+            <button class="btn btn-light" type="submit">Поиск </button>
+          </form>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default {
+        name: "Marvell",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ starting-template/src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <div class="text-center">
+        <div class="spinner-border text-danger" role="status">
+            <span class="sr-only"></span>
+        </div>
+    </div> -->
+  <div class="d-flex align-items-center">
+    <strong>Loading...</strong>
+    <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spinner",
+};
+</script>
+
+<style scoped></style>
