9edaeb76081a8ee004265d77498adc5ac90bc543
sergioQA <34909372+ramasQA@users.noreply.github.com>
2022-03-04 10:36:53 +0700

    Add files via upload

---
+++ precache-manifest.19464bcde454176565cdc1d3d33b2853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71cb66d7270f7fd3ed0e0678c2cad76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029dab06bd33b6036d3d",
+    "url": "/js/app.74b91617.js"
+  },
+  {
+    "revision": "c87948b7609b0547cf83",
+    "url": "/js/chunk-vendors.da1d1428.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
